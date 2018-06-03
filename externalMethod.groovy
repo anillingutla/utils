d6bf428a620122e05ea4f2ed0a1529c5565925cc
anillingutla <28605906+anillingutla@users.noreply.github.com>
2018-06-03 09:18:43 -0500

    Update externalMethod.groovy

--- externalMethod.groovy
+++ externalMethod.groovy
@@ -1,10 +1,9 @@
 #!groovy
 
-def myVar = build.getEnvironment(listener).get('myVar')
-
 // Methods in this file will end up as object methods on the object that load returns.
-def lookAtThis(String whoAreYou) {
-   echo "Look at this, ${whoAreYou}! You loaded this from another file!"
+def call(def str) {
+   //def myVar = build.getEnvironment(listener).get('myVar')
+   echo "Look at this, ${str}! You loaded this from another file!"
 }
    
 return this;
