556b8430c86bd14fe1a3a8157e0be8e7fd6657d0
anillingutla <28605906+anillingutla@users.noreply.github.com>
2018-06-04 09:15:49 -0500

    Delete get_creds.groovy

--- vars/get_creds.groovy
+++
@@ -1,9 +0,0 @@
-def creds = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
-      com.cloudbees.plugins.credentials.common.StandardUsernameCredentials.class,
-      Jenkins.instance,
-      null,
-      null
-  );
-for (c in creds) {
-println(c.id + ": " + c.description)
-}
